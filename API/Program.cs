efefa0cb3e47ff787cce767e2009045b2ca215e6
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2025-02-07 15:43:48 +0500

    Configure MediatR, AutoMapper and Infrastructure

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using Infrastructure;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssemblies(typeof(Program).GetTypeInfo().Assembly));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
