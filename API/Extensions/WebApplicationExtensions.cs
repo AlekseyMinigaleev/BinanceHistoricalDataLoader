aef543070b618c77b3048290e6bd8a905e9c07a3
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2025-02-09 19:48:21 +0500

    add logs by serilog

--- API/Extensions/WebApplicationExtensions.cs
+++ API/Extensions/WebApplicationExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Infrastructure.MongoDb.Services.MongoDbInitializer;
+using Serilog;
 
 namespace API.Extensions
 {
@@ -7,10 +8,16 @@ namespace API.Extensions
         public static async Task InitApp(
             this WebApplication app)
         {
+            Log.Information("Starting application initialization.");
+
             using var scope = app.Services.CreateScope();
             var serviceProvider = scope.ServiceProvider;
 
+            Log.Information("Initializing database");
             await InitMongoDbAsync(serviceProvider);
+            Log.Information("Database initialization complete.");
+
+            Log.Information("Application initialization finished successfully.");
         }
 
         private static async Task InitMongoDbAsync(
--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,15 @@
 using API.Extensions;
 using Hangfire;
 using Infrastructure;
+using Serilog;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -12,6 +18,7 @@ builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssemblies(typeof(Program).GetTypeInfo().Assembly));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
