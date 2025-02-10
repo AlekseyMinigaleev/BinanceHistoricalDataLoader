b1315cc7cf7ce6e0b54aebd8db834a41c95c44f5
Минигалеев Алексей Евгеньевич <a.minigaleev@k-soft-spb.ru>
2025-02-10 13:52:37 +0500

    fix exception text

--- Domain/Exceptions/MissingApiStringAttributeException.cs
+++ Domain/Exceptions/MissingApiStringAttributeException.cs
@@ -2,7 +2,7 @@
 {
     public class MissingApiStringAttributeException(
         string interval)
-        : Exception($"The interval value '{interval}' does not have the required [EnumMember] attribute.")
+        : Exception($"The interval value '{interval}' does not have the required [Description] attribute.")
     {
         public string Interval { get; } = interval;
     }
