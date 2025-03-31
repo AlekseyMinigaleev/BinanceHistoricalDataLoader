772a7b5f1522e39f10488abf98a9d446bc8281a7
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2025-03-31 19:54:23 +0500

    add LoadHistoricalData_HappyPath_ShouldCreateReportAndKlinesAndUpdateJob test

--- Tests/IntegrationTests/ServicesTests/HangfireTests/JobTets/LoadHistoricalDataJobTests/LoadHistoricalDataJobTests.cs
+++ Tests/IntegrationTests/ServicesTests/HangfireTests/JobTets/LoadHistoricalDataJobTests/LoadHistoricalDataJobTests.cs
@@ -5,8 +5,6 @@ using FluentAssertions;
 using Infrastructure.Hangfire.Jobs.LoadHistoricalDataJob;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
-using Tests.IntegrationTests.HttpUtils.Klines;
-using Tests.IntegrationTests.HttpUtils.Klines.Klines;
 using Job = Domain.Models.Job.Job;
 
 namespace Tests.IntegrationTests.ServicesTests.HangfireTests.JobTets.LoadHistoricalDataJobTests
@@ -18,8 +16,9 @@ namespace Tests.IntegrationTests.ServicesTests.HangfireTests.JobTets.LoadHistori
         private readonly TestWebApplicationFactory _app = app;
 
         [Fact]
-        public async Task LoadHistoricalDataAsync_HappyPath_ShouldCreateReportAndKlinesAndUpdateJob()
+        public async Task LoadHistoricalData_HappyPath_ShouldCreateReportAndKlinesAndUpdateJob()
         {
+            //arrange
             using var client = _app.CreateClient();
             using var scope = _app.Services.CreateScope();
 
@@ -27,62 +26,79 @@ namespace Tests.IntegrationTests.ServicesTests.HangfireTests.JobTets.LoadHistori
             var db = scope.ServiceProvider.GetRequiredService<IMongoDatabase>();
             var httpClientFactory = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>();
 
-            var httpUtils = new KlinesHttpUtils(
-                httpClientFactory);
+            var expectedJob = CreateJob();
+            await InsertJobToDbAsync(expectedJob, db);
 
-            var job = CreateJob();
-            await InsertJobToDbAsync(job, db);
+            //act
+            await historicalDataJob.LoadHistoricalDataAsync(expectedJob, default, default);
 
-            var candleSticksTestModel = new List<CandlestickTestModel>();
+            //assert
+            var actualJob = await GetJobFromDbAsync(db, expectedJob.Id);
+            VerifyJobIsCorrectlyModified(expectedJob, actualJob);
 
-            foreach (var symbol in job.Parameters.Symbols)
-            {
-                var parameters = new Parameters(
-                    symbol: symbol,
-                    startDate: job.Parameters.StartDate,
-                    endDate: job.Parameters.EndDate,
-                    interval: Interval.OneDay);
+            var actualReport = await GetReportFromDbAsync(db, actualJob!.ReportId!.Value);
+            VerifyReportIsValid(actualReport, actualJob.Parameters);
 
-                var klines = await httpUtils.FetchDataAsync(parameters, default);
-
-                candleSticksTestModel.Add(new CandlestickTestModel(parameters, klines));
-            }
-
-            await historicalDataJob.LoadHistoricalDataAsync(job, default, default);
-
-            var updatedJob = await GetJobAsync(db, job.Id);
-            Assert.NotNull(updatedJob);
-            Assert.NotNull(updatedJob.ReportId);
-            Assert.NotEqual(Guid.Empty, updatedJob.ReportId);
-
-            var report = await GetReportAsync(db, updatedJob.ReportId.Value);
-            Assert.NotNull(report);
-            Assert.Equal(report.Candlesticks.Count, job.Parameters.Symbols.Count);
-
-            var actrualSymbols = report.Candlesticks
-                .Select(x => x.Symbol)
-                .ToList();
-            var expectedSymbols = candleSticksTestModel
-                .Select(x => x.Parameters.Symbol)
-                .ToList();
-
-            actrualSymbols.Should().BeEquivalentTo(expectedSymbols);
-
-            foreach (var candlestick in report.Candlesticks)
-            {
-
-            }
+            await VerifyKlinesValidCountAsync(
+                actualJob.Parameters,
+                [.. actualReport!.Candlesticks],
+                db);
         }
 
         private Job CreateJob()
         {
             var jobParameters = new JobParameters(
-               ["EURUSD", "USDJPY"],
+               ["BTCUSDT", "ETHUSDT"],
                DateTime.UtcNow.AddDays(-1),
                DateTime.UtcNow);
             var job = new Job(jobParameters);
 
             return job;
+        }
+
+        private void VerifyJobIsCorrectlyModified(Job expected, Job? actual)
+        {
+            Assert.NotNull(actual);
+            expected.Parameters.Should().BeEquivalentTo(actual.Parameters);
+            Assert.NotNull(actual.ReportId);
+            Assert.NotEqual(Guid.Empty, actual.ReportId);
+            Assert.Null(actual.ErrorMessage);
+            Assert.Equal(JobStatus.Completed, actual.Status);
+            var emptyDateTime = new DateTime();
+            Assert.NotNull(actual.StartTime);
+            Assert.NotEqual(emptyDateTime, actual.StartTime);
+            Assert.NotNull(actual.EndTime);
+            Assert.NotEqual(emptyDateTime, actual.EndTime);
+        }
+
+        private void VerifyReportIsValid(
+            Report? actualReport,
+            JobParameters parameters)
+        {
+            Assert.NotNull(actualReport);
+            Assert.Equal(parameters.Symbols.Count, actualReport.Candlesticks.Count);
+
+            var actualSymbols = actualReport.Candlesticks
+                .Select(x => x.Symbol)
+                .ToList();
+
+            actualSymbols
+                .Should()
+                .BeEquivalentTo(parameters.Symbols);
+        }
+
+        private async Task VerifyKlinesValidCountAsync(
+            JobParameters jobParameters,
+            List<Candlestick> actualCandlesticks,
+            IMongoDatabase db)
+        {
+            var expectedKlinesCount = (jobParameters.StartDate - jobParameters.EndDate).Days;
+
+            foreach (var actualCandlestick in actualCandlesticks)
+            {
+                var actualKlines = await GetKlinesAsync(db, [.. actualCandlestick.KlineIds]);
+                Assert.Equal(expectedKlinesCount, actualKlines.Count);
+            }
         }
 
         private async Task InsertJobToDbAsync(Job job, IMongoDatabase db)
@@ -91,7 +107,7 @@ namespace Tests.IntegrationTests.ServicesTests.HangfireTests.JobTets.LoadHistori
             await jobs.InsertOneAsync(job);
         }
 
-        private async Task<Job?> GetJobAsync(
+        private async Task<Job?> GetJobFromDbAsync(
             IMongoDatabase db,
             Guid jobId)
         {
@@ -104,7 +120,7 @@ namespace Tests.IntegrationTests.ServicesTests.HangfireTests.JobTets.LoadHistori
             return job;
         }
 
-        private async Task<Report?> GetReportAsync(
+        private async Task<Report?> GetReportFromDbAsync(
             IMongoDatabase db,
             Guid reportId)
         {
@@ -119,7 +135,7 @@ namespace Tests.IntegrationTests.ServicesTests.HangfireTests.JobTets.LoadHistori
 
         private async Task<List<Kline>> GetKlinesAsync(
             IMongoDatabase db,
-            List<Guid>klineIds)
+            List<Guid> klineIds)
         {
             var klines = db.GetCollection<Kline>(nameof(Kline));
             var filter = Builders<Kline>.Filter.In(k => k.Id, klineIds);
@@ -131,3 +147,26 @@ namespace Tests.IntegrationTests.ServicesTests.HangfireTests.JobTets.LoadHistori
         }
     }
 }
+
+
+//private async Task<List<CandlestickTestModel>> FetchExpectedCandlesticksAsync(
+//            Job expectedJob,
+//            IHttpClientFactory httpClientFactory)
+//{
+//    var httpUtils = new KlinesHttpUtils(httpClientFactory);
+//    var expectedCandlesticks = new List<CandlestickTestModel>();
+//    foreach (var symbol in expectedJob.Parameters.Symbols)
+//    {
+//        var parameters = new Parameters(
+//            symbol: symbol,
+//            startDate: expectedJob.Parameters.StartDate,
+//            endDate: expectedJob.Parameters.EndDate,
+//            interval: Interval.OneDay);
+
+//        var klines = await httpUtils.FetchDataAsync(parameters, default);
+
+//        expectedCandlesticks.Add(new CandlestickTestModel(parameters, klines));
+//    }
+
+//    return expectedCandlesticks;
+//}
